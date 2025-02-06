792de727b0787870063fe6fc9dc454d4b7ec711b
Stephen Hodgson <Stephen@srhodgson.co.uk>
2025-02-06 20:07:11 +0000

    Add the obligatory hello world program

---
+++ src/chapterOne/MyFirstApp.java
@@ -0,0 +1,9 @@
+package chapterOne;
+
+public class MyFirstApp {
+
+    public static void main(String[] args) {
+        System.out.println("I Rule!");
+        System.out.println("The World");
+    }
+}

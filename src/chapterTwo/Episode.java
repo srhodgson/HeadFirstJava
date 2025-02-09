c1822f76dfba9a6874723fa7341400978daf4e29
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-02-09 22:47:26 +0000

    episode before fixes

---
+++ src/chapterTwo/Episode.java
@@ -0,0 +1,15 @@
+package chapterTwo;
+
+public class Episode {
+
+    int seriesNumber;
+    int episodeNumber;
+
+    void skipIntro() {
+        System.out.println("Skipping intro...");
+    }
+
+    void skipToNext() {
+        System.out.println("Loading next episode...");
+    }
+}

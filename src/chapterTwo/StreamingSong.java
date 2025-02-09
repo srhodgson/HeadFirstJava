7419ba10fbca53050295702cc425cd8510b35790
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-02-09 22:35:50 +0000

    StreamingSong before fixes

---
+++ src/chapterTwo/StreamingSong.java
@@ -0,0 +1,12 @@
+package chapterTwo;
+
+public class StreamingSong {
+
+    String title;
+    String artist;
+    int duration;
+
+    void play() {
+        System.out.println("This is " + title + " by " + artist);
+    }
+}

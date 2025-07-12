31a515260d7a4b614120984c215213a8ad0d89bc
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-07-12 21:34:35 +0100

    Initialise project

---
+++ src/Chapter01/MyFirstApp.java
@@ -0,0 +1,9 @@
+package Chapter01;
+
+public class MyFirstApp {
+
+    public static void main(String[] args) {
+        System.out.println("I Rule!");
+        System.out.println("The World!");
+    }
+}

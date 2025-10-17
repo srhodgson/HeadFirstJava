c55ff50fb88be65730c388742d88c48b7b65ab72
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-10-17 22:36:21 +0100

    Chapter03
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

---
+++ src/Chapter03/TestArrays.java
@@ -0,0 +1,28 @@
+package Chapter03;
+
+public class TestArrays {
+    public static void main(String[] args) {
+        String[] islands = new String[4];
+        int[] index = new int[4];
+        int ref;
+        int y = 0;
+
+        islands[0] = "Bermuda";
+        islands[1] = "Fiji";
+        islands[2] = "Azores";
+        islands[3] = "Cozumel";
+
+        index[0] = 1;
+        index[1] = 3;
+        index[2] = 0;
+        index[3] = 2;
+
+        while (y < 4) {
+            ref = index[y];
+
+            System.out.print("island = ");
+            System.out.println(islands[ref]);
+            y = y + 1;
+        }
+    }
+}

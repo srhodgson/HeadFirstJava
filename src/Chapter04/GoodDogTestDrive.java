72f216e842ce9709e5e23b46a29526e70664b229
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-10-18 22:12:13 +0100

    Adding some more work

---
+++ src/Chapter04/GoodDogTestDrive.java
@@ -0,0 +1,13 @@
+package Chapter04;
+
+public class GoodDogTestDrive {
+
+    public static void main(String[] args) {
+        GoodDog one = new GoodDog();
+        one.setSize(70);
+        GoodDog two = new GoodDog();
+        two.setSize(8);
+
+        System.out.println("Dog one: " + one.getSize());
+    }
+}

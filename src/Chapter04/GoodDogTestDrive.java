1b405ac97735f9557364f40eae3a121695c34029
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-10-19 21:54:09 +0100

    Adding some more work

--- src/Chapter04/GoodDogTestDrive.java
+++ src/Chapter04/GoodDogTestDrive.java
@@ -9,5 +9,8 @@ public class GoodDogTestDrive {
         two.setSize(8);
 
         System.out.println("Dog one: " + one.getSize());
+        System.out.println("Dog two: " + two.getSize());
+        one.bark();
+        two.bark();
     }
 }

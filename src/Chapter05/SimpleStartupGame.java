1ae5a668768fd8052fd9036c82356d4207afa794
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-10-22 23:16:31 +0100

    StartupBust

--- src/Chapter05/SimpleStartupGame.java
+++ src/Chapter05/SimpleStartupGame.java
@@ -1,29 +1,32 @@
-package Chapter05;
-
-public class SimpleStartupGame {
-    public static void main(String[] args) {
-        int numOfGuesses = 0;
-        GameHelper helper = new GameHelper();
-
-        SimpleStartup theStartup = new SimpleStartup();
-
-        int randomNum = (int) (Math.random() * 5);
-
-        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
-
-        theStartup.setLocationCells(locations);
-
-        boolean isAlive = true;
-
-        while (isAlive) {
-            int guess = helper.getUserInput("enter a number");
-            String result = theStartup.checkYourself(guess);
-            numOfGuesses++;
-
-            if (result.equals("kill")) {
-                isAlive = false;
-                System.out.println("You took " + numOfGuesses + " guesses.");
-            }
-        }
-    }
-}
+//package Chapter05;
+//
+//import java.util.ArrayList;
+//
+//public class SimpleStartupGame {
+//    public static void main(String[] args) {
+//        int numOfGuesses = 0;
+//        GameHelper helper = new GameHelper();
+//
+//        SimpleStartup theStartup = new SimpleStartup();
+//
+//        int randomNum = (int) (Math.random() * 5);
+//
+//        ArrayList<String> locations = new ArrayList<>();
+//
+//        theStartup.setLocationCells(locations);
+//
+//        boolean isAlive = true;
+//
+//        while (isAlive) {
+//            int guess = helper.getUserInput("enter a number");
+//            String result = theStartup.checkYourself(String.valueOf(guess));
+//            numOfGuesses++;
+//
+//            if (result.equals("kill")) {
+//                isAlive = false;
+//                System.out.println("You took " + numOfGuesses + " guesses.");
+//            }
+//        }
+//    }
+//}
+//
---
+++ src/Chapter06/Startup.java
@@ -0,0 +1,31 @@
+package Chapter06;
+import java.util.ArrayList;
+
+public class Startup {
+    private ArrayList<String> locationCells;
+    private String name;
+
+    public void setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String checkYourSelf(String userInput) {
+        String result = "miss";
+        int index = locationCells.indexOf(userInput);
+        if (index >= 0) {
+            locationCells.remove(index);
+
+            if (locationCells.isEmpty()) {
+                result = "kill";
+                System.out.println("Ouch! You sunk " + name + " : ( ");
+            } else {
+                result = "hit";
+            }// end if
+        }// end outer if
+        return result;
+    } // end method
+}
